3f307bc7473ee8c251c123826fa1aeddd98647b2
Dan Higham <dan.higham@gmail.com>
2016-01-26 15:53:13 -0800

    Change auth token for github repo

--- launch_script.sh
+++ launch_script.sh
@@ -2,7 +2,7 @@ mkdir manifests
 
 for f in bosh.yml manifests/index.yml setup_dns.py create_cert.sh setup_devbox.py init.sh deploy_bosh.sh 98-msft-love-cf
 do
-   curl --header 'Authorization: token d817959d53b8c53b5bf1710cd1be5079277db8ad' \
+   curl --header 'Authorization: token 6b36115cecd0a1b748024a853999aabda62b7644' \
         --header 'Accept: application/vnd.github.v3.raw' \
         --remote-name \
         --location https://api.github.com/repos/cf-platform-eng/bosh-azure-template/contents/$f \
