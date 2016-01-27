34700ec913a44fc0b9df4c28391dc1da8de57df1
Dan Higham <dan.higham@gmail.com>
2016-01-27 15:13:41 -0800

    Target bosh automatically

--- deploy_bosh_and_releases.py
+++ deploy_bosh_and_releases.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
+import yaml
 from subprocess import call
+from subprocess import Popen, PIPE
 
 home_dir = os.environ["HOME"]
 install_log = os.path.join(home_dir, "install.log")
@@ -9,4 +11,26 @@ install_log = os.path.join(home_dir, "install.log")
 os.environ["BOSH_INIT_LOG_LEVEL"] = 'INFO'
 os.environ["BOSH_INIT_LOG_PATH"] = './bosh-init-debug.log'
 
-call("bosh-init deploy bosh.yml", shell=True)
+# do we have a targeted bosh?
+p = Popen(['bosh', 'target'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+output, err = p.communicate()
+
+# assume if no bosh is target, there is no director
+if "Target not set" in output:
+    call("bosh-init deploy bosh.yml", shell=True)
+
+    p = Popen(['bosh', 'target', '10.0.0.4'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"admin\r\nadmin\r\n")
+    rc = p.returncode
+
+# load manifests in to memory
+f = open('manifests/index.yml')
+manifests = yaml.safe_load(f)
+f.close()
+
+# process releases
+#m_list = []
+#for m in manifests['manifests']:
+#    m_list.append("manifests/{0}".format(m['file']))
+
+#m_list.append('bosh.yml')
--- setup_devbox.py
+++ setup_devbox.py
@@ -85,9 +85,10 @@ for template in m_list:
 
 # Copy all the files in ./bosh into the home directory
 call("cp -r ./bosh/* {0}".format(home_dir), shell=True)
+call("cp ./deploy_bosh_and_releases.py ./manifests/index.yml {0}".format(home_dir), shell=True)
+
 call("chown -R {0} {1}".format(username, home_dir), shell=True)
 call("chmod 400 {0}/bosh".format(home_dir), shell=True)
-call("cp ./deploy_bosh_and_releases.py {0}".format(home_dir), shell=True)
 
 #Install bosh_cli and bosh-init
 #call("rm -r /tmp; mkdir /mnt/tmp; ln -s /mnt/tmp /tmp; chmod 777 /mnt/tmp; chmod 777 /tmp", shell=True)
