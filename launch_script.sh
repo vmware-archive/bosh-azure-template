1ed884ad820f17c3cf7eca8d9a7ea99ca7f34535
Dan Higham <dan.higham@gmail.com>
2016-01-26 18:36:06 -0800

    Remove unnecessary properties from cf.yml

--- launch_script.sh
+++ launch_script.sh
@@ -1,6 +1,6 @@
 mkdir manifests
 
-for f in bosh.yml "manifests/index.yml" setup_dns.py create_cert.sh setup_devbox.py init.sh deploy_bosh.sh 98-msft-love-cf
+for f in bosh.yml manifests/index.yml setup_dns.py create_cert.sh setup_devbox.py init.sh deploy_bosh.sh 98-msft-love-cf
 do
    curl --silent \
         https://raw.githubusercontent.com/cf-platform-eng/bosh-azure-template/master/$f \
