f3893321b9748bd3a1e72f35a4d43d0eef70b8c3
Dan Higham <dan.higham@gmail.com>
2016-01-28 14:44:00 -0800

    Fix path for bosh.yml

--- deploy_bosh_and_releases.py
+++ deploy_bosh_and_releases.py
@@ -29,7 +29,7 @@ output, err = p.communicate()
 
 # assume if no bosh is target, there is no director
 if "Target not set" in err:
-    call("bosh-init deploy bosh.yml", shell=True)
+    call("bosh-init deploy ./bosh/bosh.yml", shell=True)
 
     p = Popen(['bosh', 'target', '10.0.0.4'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate(b"admin\r\nadmin\r\n")
