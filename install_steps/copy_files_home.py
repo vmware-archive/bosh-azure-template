70c790e550369ed6318af6d819b67ba5c0bfd67e
Jeenal Shah <jshah@pivotal.io>
2016-02-16 10:30:00 -0800

    import necessary libraries

--- install_steps/copy_files_home.py
+++ install_steps/copy_files_home.py
@@ -1,3 +1,9 @@
+import os
+
+from subprocess import call
+from distutils import dir_util
+from shutil import copy
+
 def do_step(context):
     settings = context.meta['settings']
 
@@ -5,8 +11,8 @@ def do_step(context):
     home_dir = os.path.join("/home", username)
 
     # Copy all the files in ./bosh into the home directory
-    call("cp -r ./bosh/* {0}".format(home_dir), shell=True)
-    call("cp ./manifests/index.yml {0}/manifests/".format(home_dir), shell=True)
+    dir_util.copy_tree("./bosh/", home_dir)
+    copy("./manifests/index.yml", "{0}/manifests/".format(home_dir))
 
     call("chown -R {0} {1}".format(username, home_dir), shell=True)
     call("chmod 400 {0}/bosh".format(home_dir), shell=True)
