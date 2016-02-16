2c6ff2b65ea291816221fe996fb282c2c4a74dd7
Dan Higham <dan.higham@gmail.com>
2016-02-16 07:07:11 -0800

    Use python libs to do file operations

--- install_steps/create_bosh_cert.py
+++ install_steps/create_bosh_cert.py
@@ -1,16 +1,19 @@
+from subprocess import call
+from os import makedirs
+from shutil import copy
+
 def do_step(context):
 
-    call("mkdir -p ./bosh", shell=True)
-    call("mkdir -p ./bosh/manifests", shell=True)
+    makedirs("bosh/manifests")
 
     # Generate the private key and certificate
     call("sh create_cert.sh", shell=True)
-    call("cp bosh.key ./bosh/bosh", shell=True)
+    copy("bosh.key", "./bosh/bosh")
     with open ('bosh_cert.pem', 'r') as tmpfile:
         ssh_cert = tmpfile.read()
     ssh_cert = "|\n" + ssh_cert
     ssh_cert="\n        ".join([line for line in ssh_cert.split('\n')])
 
     context.meta['settings']['SSH_CERTIFICATE'] = ssh_cert
-    
+
     return context
